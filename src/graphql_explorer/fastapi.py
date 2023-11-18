1ac1e48bcbbb3c9a53ccf0da5beb244f240cc8e9
Abhinand C <abhinandc.ac@gmail.com>
2023-11-18 19:15:22 +0530

    feat: Add initial code, bump version

---
+++ src/graphql_explorer/fastapi.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+from .templates import APOLLO_SANDBOX_HTML, GRAPHIQL_HTML, GRAPHQL_VOYAGER_HTML
+
+try:
+    import logging
+    uvicorn_logger = logging.getLogger("uvicorn.error")
+    uvicorn_logger.info(
+        "GraphQL Explorer [FastAPI] enabled!"
+        "\n\t Apollo Sandbox Explorer at:\t /  &  /explorer/apollo"
+        "\n\t GraphiQL Playground at:\t /explorer/graphiql"
+        "\n\t GraphQL Voyager at:\t\t /explorer/voyager"
+        ""
+    )
+except Exception:
+    pass
+
+router = APIRouter(tags=["GraphQL Router"])
+
+@router.get("/explorer/apollo", name="Apollo Sandbox")
+@router.get("/", name="Apollo Sandbox")
+async def apollo_sandbox() -> HTMLResponse:
+    return HTMLResponse(
+        content=APOLLO_SANDBOX_HTML
+    )
+
+
+@router.get("/explorer/graphiql", name="GraphiQL Playground")
+async def graphiql() -> HTMLResponse:
+    return HTMLResponse(
+        content=GRAPHIQL_HTML
+    )
+
+@router.get("/explorer/voyager", name="GraphQL Voyager")
+async def voyager() -> HTMLResponse:
+    return HTMLResponse(
+        content=GRAPHQL_VOYAGER_HTML
+    )
---
+++ src/graphql_explorer/templates/__init__.py
@@ -0,0 +1,4 @@
+
+from .apollo_sandbox import APOLLO_SANDBOX_HTML
+from .graphiql import GRAPHIQL_HTML
+from .voyager import GRAPHQL_VOYAGER_HTML
